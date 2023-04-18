cf3fbe4b802fb758f92c27f774828758798bfede
Pongsapak Waiwitlikhit <42717499+porwai@users.noreply.github.com>
2023-04-18 00:50:52 -0400

    Delete CraftingInterpreters directory

--- CraftingInterpreters/javaInterpretor/com/craftinginterpreters/lox/LoxCallable.java
+++
@@ -1,9 +0,0 @@
-package com.craftinginterpreters.lox;
-
-import java.util.List;
-
-public interface LoxCallable {
-    int arity();
-
-    Object call(Interpreter interpreter, List<Object> arguments);
-}
